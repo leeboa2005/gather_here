ce881ce332f1f240398013b9a00788eb47111e26
kybaq <flfsk12@gmail.com>
2024-07-19 16:30:20 +0900

    init: 프로젝트 초기화

---
+++ src/Provider/ContextProvider.tsx
@@ -0,0 +1,11 @@
+// context Provider for the app
+
+import { createContext } from "react";
+
+const context = createContext({}); // default value
+
+const ContextProvider = () => {
+  //   return <context.Provider value={}>ContextProvider</context.Provider>;
+};
+
+export default ContextProvider;
