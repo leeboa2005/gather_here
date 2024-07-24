43e07249774a997918fc2e6db4406201c3070a56
kybaq <flfsk12@gmail.com>
2024-07-24 11:17:01 +0900

    style: 캘린더 부모 컨테이너 임시 추가

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,7 +4,9 @@ import React from "react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Calender />
+      <div className="w-96">
+        <Calender />
+      </div>
     </main>
   );
 }
