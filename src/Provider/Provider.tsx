d1343eb3c4259383cd311bbdac7add3fd8905ef4
kybaq <flfsk12@gmail.com>
2024-07-24 10:51:50 +0900

    del: 파일 이름 변경

--- src/Provider/Provider.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import QueryClientProvider from "./QueryClientProvider";
-
-const Provider = ({ children }: PropsWithChildren) => {
-  return <QueryClientProvider>{children}</QueryClientProvider>;
-};
-
-export default Provider;
