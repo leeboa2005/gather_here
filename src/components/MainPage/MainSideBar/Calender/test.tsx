b78c780d2784209e8c169af2bcd06571d1ca64a2
kybaq <flfsk12@gmail.com>
2024-07-24 11:12:03 +0900

    move: 파일 이름 변경

---
+++ src/components/MainPage/MainSideBar/Calender/test.tsx
@@ -0,0 +1,144 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="frame-wrapper">
+        <div className="div">
+          <img className="img" alt="Frame" src="frame-427321211.svg" />
+          <div className="text-wrapper">2024년 8월</div>
+          <img className="img" alt="Frame" src="frame-427321210.svg" />
+        </div>
+      </div>
+      <div className="div-2">
+        <div className="div-3">
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">M</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">T</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">W</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">T</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">F</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">S</div>
+          </div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-2">S</div>
+          </div>
+        </div>
+        <div className="div-3">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">1</div>
+          </div>
+          <div className="div-wrapper-3">
+            <div className="text-wrapper-4">2</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">3</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">4</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">5</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">6</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">7</div>
+          </div>
+        </div>
+        <div className="div-3">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">8</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">9</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">10</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">11</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">12</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">13</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">14</div>
+          </div>
+        </div>
+        <div className="div-3">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">15</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">16</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">17</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">18</div>
+          </div>
+          <div className="div-wrapper-3">
+            <div className="text-wrapper-4">19</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">20</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">21</div>
+          </div>
+        </div>
+        <div className="div-3">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">22</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">23</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">24</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">25</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">26</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">27</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">28</div>
+          </div>
+        </div>
+        <div className="div-4">
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">29</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">30</div>
+          </div>
+          <div className="div-wrapper-2">
+            <div className="text-wrapper-3">31</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
