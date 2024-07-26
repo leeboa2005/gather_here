531fd81d4f15eb046990daae265c4252ee316182
kybaq <flfsk12@gmail.com>
2024-07-26 11:43:20 +0900

    feat: 채팅 기능추가 #20

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Calender from "@/components/MainPage/MainSideBar/Calender/Calender";
+import Chat from "@/components/MainPage/MainSideBar/Chat/Chat";
 import React from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Calender />
+      <Chat />
     </main>
   );
 }
