33fb679e893f342bcdf3cd11b91b95a57736c122
LeeHareum <g029843507@gmail.com>
2024-07-26 20:59:02 +0900

    style:와이어프레임기준준UI틀,add:캐러셀스타일,필터바 #15

---
+++ src/components/MainPage/PageContent/ProjectContent.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchDeadlinePosts } from "@/lib/fetchDeadlinePosts";
+import { Post } from "@/types/posts/Post.type";
+import PostCardLong from "@/components/Common/Card/PostCard/PostCardLong";
+import AdCard from "@/components/MainPage/AdCard/AdCard";
+import { fetchPostsWithDeadLine } from "@/lib/fetchPosts";
+
+const Carousel = dynamic(() => import("@/components/MainPage/Carousel/Carousel"), { ssr: false });
+
+interface ProjectContentProps {
+  initialPosts: Post[];
+}
+
+const ProjectContent: React.FC<ProjectContentProps> = ({ initialPosts }) => {
+  const [posts, setPosts] = useState<Post[]>(initialPosts);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(1);
+  // 캐러셀용 상태추가하고
+
+  useEffect(() => {
+    const loadCarouselData = async () => {
+      await fetchPostsWithDeadLine();
+    };
+    loadCarouselData();
+  }, []); // 캐러셀용 따로
+
+  const loadMorePosts = async () => {
+    const newPosts: Post[] = await fetchDeadlinePosts(page, "프로젝트", 50); // 필요한 날짜 범위로 변경
+
+    if (!newPosts || newPosts.length === 0) {
+      setHasMore(false);
+      return;
+    }
+
+    setPosts((prevPosts) => [...prevPosts, ...newPosts]);
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <div>
+      <h1 className="font-bold text-lg">마감 임박</h1>
+      <Carousel posts={posts} />
+      <InfiniteScroll
+        dataLength={posts.length}
+        next={loadMorePosts}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+        endMessage={<p style={{ textAlign: "center" }}>모든 포스트를 불러왔습니다.</p>}
+      >
+        {posts.map((post, index) => (
+          <React.Fragment key={`${post.post_id}_${index}`}>
+            <PostCardLong post={post} />
+            {(index + 1) % 5 === 0 && <AdCard key={`ad_${index}`} />}
+          </React.Fragment>
+        ))}
+      </InfiniteScroll>
+    </div>
+  );
+};
+
+export default ProjectContent;
