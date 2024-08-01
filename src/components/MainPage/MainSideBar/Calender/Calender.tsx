b028e2c73b21df72177b69cf1368352034ad749e
kybaq <flfsk12@gmail.com>
2024-08-01 21:00:39 +0900

    style: 반응형 제외 디자인 시안 반영 #49

--- src/components/MainPage/MainSideBar/Calender/Calender.tsx
+++ src/components/MainPage/MainSideBar/Calender/Calender.tsx
@@ -8,12 +8,8 @@ import { useQuery } from "@tanstack/react-query";
 import dayjs from "dayjs";
 import { useEffect, useRef, useState } from "react";
 import "./fullcalender.css";
-
-type EventsType = {
-  title: string;
-  date_start: string;
-  date_done: string;
-};
+import { NextPage } from "next";
+import { Tables } from "@/types/supabase";
 
 const detectMobileDevice = () => {
   const mobileWidth = 1068;
@@ -21,7 +17,7 @@ const detectMobileDevice = () => {
   return innerWidth <= mobileWidth;
 }; // UserAgent 이용해야할지 ?
 
-const Calender: React.FC = () => {
+const Calender: NextPage = () => {
   const calenderRef = useRef<FullCalendar | null>(null); // 캘린더 객체 내부의 메서드를 사용하기 위해 정의함, | null 인 이유는 렌더링 이전에는 null 인 상태기 때문에 안정성을 위해 추가.
   const [isGrid, setIsGrid] = useState(true);
   const [isMobileDevice, setIsMobileDevice] = useState(false);
@@ -39,10 +35,11 @@ const Calender: React.FC = () => {
     if (response.ok) {
       const { data } = await response.json(); // 캘린더에 표기하기 위해 가져온 행사 일정의 날짜 형식을 바꿔줘야 할 수도 있음. start, end => yyyy-mm-dd 로만 바꾸면 될 듯
 
-      const events = data.map((event: EventsType) => ({
+      const events = data.map((event: Tables<"IT_Events">) => ({
         title: event.title,
         start: dayjs(event.date_start).format("YYYY-MM-DD"), // yyyy-mm-dd 로 바꿔줘야함
         end: dayjs(event.date_done).format("YYYY-MM-DD"),
+        description: event.location,
       }));
 
       return events;
@@ -60,6 +57,20 @@ const Calender: React.FC = () => {
     queryFn: getEvents,
   });
 
+  useEffect(() => {
+    // FullCalendar가 렌더링된 후에 실행됩니다.
+    if (calenderRef) {
+      const aElements = document.querySelectorAll(".fc-daygrid-day-bottom");
+      console.log(aElements);
+      aElements.forEach((aEl) => {
+        // 예를 들어, 클릭 이벤트를 추가할 수 있습니다.
+        aEl.addEventListener("click", () => {
+          console.log("Anchor clicked!");
+        });
+      });
+    }
+  }, [calenderRef]);
+
   if (isPending) {
     return <div>Loading</div>;
   }
@@ -71,14 +82,13 @@ const Calender: React.FC = () => {
     <>
       <h4 className="mb-4">HOT한 IT 행사 놓치지 마세요</h4>
       <FullCalendar
-        plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
-        aspectRatio={0.65} // 큰 값으로 설정하면 더 작게 보임
-        ref={calenderRef}
+        // 공통 옵션
+        aspectRatio={0.678} // 큰 값으로 설정하면 더 작게 보임
         customButtons={{
           viewChanger: {
             click() {
               if (isGrid) {
-                calenderRef.current?.getApi().changeView("list");
+                calenderRef.current?.getApi().changeView("listMonth");
                 setIsGrid(false);
               } else {
                 calenderRef.current?.getApi().changeView("dayGridMonth");
@@ -87,27 +97,11 @@ const Calender: React.FC = () => {
             },
           },
         }}
-        // dateClick={(info) => {
-        //   console.log(info);
-        //   alert("Current view: " + info.view);
-        // }}
-        firstDay={1}
-        initialView={!isMobileDevice ? "dayGridMonth" : "list"}
-        eventSources={[
-          {
-            events: IT_Events,
-            color: "black",
-            textColor: "yellow",
-          },
-        ]}
-        dayMaxEventRows={0}
-        dayCellContent={(cellContent) => {
-          return cellContent.dayNumberText.replace("일", "");
-        }}
-        moreLinkContent={(asdf) => {
-          return asdf.shortText;
-        }}
-        eventDisplay="list-item"
+        plugins={[dayGridPlugin, listPlugin, interactionPlugin]}
+        ref={calenderRef}
+        initialView={!isMobileDevice ? "dayGridMonth" : "listMonth"}
+        locale="ko"
+        events={IT_Events}
         headerToolbar={
           isMobileDevice
             ? {
@@ -118,9 +112,35 @@ const Calender: React.FC = () => {
             : { start: "title", end: "prev next viewChanger" }
         }
         titleFormat={(date) => {
-          return date.date.year + "년 " + (date.date.month + 1) + "월";
+          return date.date.year + ". " + String(date.date.month + 1).padStart(2, "0");
         }}
-        locale="ko"
+        // Grid View 옵션
+        firstDay={1}
+        dayMaxEventRows={0}
+        dayCellContent={(cellContent) => {
+          return cellContent.dayNumberText.replace("일", "");
+        }}
+        moreLinkContent={(arg) => {
+          return arg.shortText;
+        }}
+        // List View 옵션
+        allDayText=""
+        displayEventTime={false}
+        eventDisplay="list-item"
+        // 장소 정보 표시
+        eventContent={(arg) => {
+          const title = document.createElement("div");
+          title.classList.add("fc-event-title");
+          title.innerHTML = arg.event.title;
+
+          const description = document.createElement("div");
+          description.classList.add("fc-event-description");
+          description.innerHTML = arg.event.extendedProps.description;
+
+          const arrayOfDomNodes = [title, description];
+
+          return { domNodes: arrayOfDomNodes };
+        }}
       />
     </>
   );
