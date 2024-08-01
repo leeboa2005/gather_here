94c3500d57b2f48d3fbf72061e683c81f80f18c2
ilovezerocokeya <147008077a@gmail.com>
2024-08-01 11:17:26 +0900

    update: 로딩 #62

--- src/components/Common/Loading/LottiAnimation.tsx
+++ src/components/Common/Loading/LottiAnimation.tsx
@@ -3,17 +3,21 @@ import React from "react";
 import Lottie from "react-lottie-player";
 import animationData from "../../../assets/LoadingAnimation.json";
 
-const LottiAnimation = () => {
+interface LottiAnimationProps {
+  size?: string;
+  isFixed?: boolean;
+  className?: string;
+}
+
+const LottiAnimation: React.FC<LottiAnimationProps> = ({ size = '100%', isFixed = false, className = '' }) => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[#fafaf8]">
-      <div className="relative w-64 h-64 sm:w-96 sm:h-96 lg:w-128 lg:h-128">
-        <Lottie
-          loop
-          animationData={animationData}
-          play
-          style={{ width: '100%', height: '100%' }}
-        />
-      </div>
+    <div className={`${isFixed ? 'fixed inset-0' : 'relative'} flex items-center justify-center bg-background ${className}`} style={{ width: size, height: size }}>
+      <Lottie
+        loop
+        animationData={animationData}
+        play
+        style={{ width: '100%', height: '100%' }}
+      />
     </div>
   );
 };
--- src/components/MainPage/InfiniteScroll/InfiniteScrollComponents.tsx
+++ src/components/MainPage/InfiniteScroll/InfiniteScrollComponents.tsx
@@ -1,8 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PostCardLong from "@/components/Common/Card/PostCard/PostCardLong";
 import AdCard from "@/components/MainPage/AdCard/AdCard";
+import LottiAnimation from "@/components/Common/Loading/LottiAnimation";
 import { Post } from "@/types/posts/Post.type";
 
 interface InfiniteScrollComponentProps {
@@ -12,21 +13,51 @@ interface InfiniteScrollComponentProps {
 }
 
 const InfiniteScrollComponent: React.FC<InfiniteScrollComponentProps> = ({ posts = [], hasMore, loadMorePosts }) => {
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await loadMorePosts();
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setTimeout(() => {
+          setInitialLoading(false);
+        }, 2000);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <InfiniteScroll
-      dataLength={posts.length}
-      next={loadMorePosts}
-      hasMore={hasMore}
-      loader={<h4>Loading...</h4>}
-      endMessage={<p style={{ textAlign: "center" }}>모든 포스트를 불러왔습니다.</p>}
-    >
-      {posts.map((post, index) => (
-        <React.Fragment key={`${post.post_id}_${index}`}>
-          <PostCardLong post={post} />
-          {(index + 1) % 5 === 0 && <AdCard key={`ad_${index}`} />}
-        </React.Fragment>
-      ))}
-    </InfiniteScroll>
+    <div>
+      {initialLoading ? (
+        <LottiAnimation size="200px" isFixed={true} /> // 초기 로딩 애니메이션
+      ) : (
+        <InfiniteScroll
+          dataLength={posts.length}
+          next={async () => {
+            setLoading(true);
+            await loadMorePosts();
+            setLoading(false);
+          }}
+          hasMore={hasMore}
+          loader={<div className="loader-container"><LottiAnimation size="50px" /></div>} // 무한 스크롤 로딩 애니메이션
+          endMessage={<p style={{ textAlign: "center" }}>모든 포스트를 불러왔습니다.</p>}
+        >
+          {posts.map((post, index) => (
+            <React.Fragment key={`${post.post_id}_${index}`}>
+              <PostCardLong post={post} />
+              {(index + 1) % 5 === 0 && <AdCard key={`ad_${index}`} />}
+            </React.Fragment>
+          ))}
+        </InfiniteScroll>
+      )}
+      {loading && <div className="loader-container"><LottiAnimation size="50px" /></div>} {/* 추가 로딩 애니메이션 */}
+    </div>
   );
 };
 
