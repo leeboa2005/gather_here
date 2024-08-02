54794d699cea9d8ccb95407bf30930751b7e6459
kybaq <flfsk12@gmail.com>
2024-08-02 19:20:08 +0900

    del: 사용하지 않는 컴포넌트 삭제

--- src/components/MainPage/MainSideBar/Chat/Chat.tsx
+++ src/components/MainPage/MainSideBar/Chat/Chat.tsx
@@ -2,7 +2,7 @@
 
 import { createClient } from "@/utils/supabase/client";
 import { Tables } from "@/types/supabase";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import useSignupStore from "@/store/useSignupStore";
 import Image from "next/image";
 import dayjs from "dayjs";
@@ -13,8 +13,10 @@ const Chat = () => {
   const { user } = useSignupStore();
   const [userInfo, setUserInfo] = useState<UserRow>();
   const [messages, setMessages] = useState<MessageRow[]>([]);
-  const [inputValue, setInputValue] = useState<string>("");
   const [deletedMessageId, setDeletedMessageId] = useState<string>("");
+  const [isOpen, setIsOpen] = useState<boolean>(true);
+  const editableDivRef = useRef<HTMLDivElement>(null);
+  const hiddenTextareaRef = useRef<HTMLTextAreaElement>(null);
   const supabase = createClient();
 
   // customHook 으로 따로 빼서 코드 정리?
@@ -29,6 +31,12 @@ const Chat = () => {
 
     getUserInfo();
   }, [user]);
+
+  useEffect(() => {
+    if (editableDivRef.current) {
+      editableDivRef.current.scrollTop = editableDivRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   useEffect(() => {
     // 채팅 내역 불러오기
@@ -83,22 +91,30 @@ const Chat = () => {
 
   const handleSubmit = async (evt: FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
-    const { data, error } = await supabase
-      .from("Messages")
-      .insert({
-        // channel_id 하드코딩 할 필요 없도록 해야함
-        channel_id: "214322ba-1cbd-424c-9ef1-e4b281f71675",
-        user_id: `${userInfo?.id}`,
-        content: `${inputValue}`,
-      })
-      .select("*");
 
-    if (error) {
-      console.error("에러: ", error);
+    if (editableDivRef.current && hiddenTextareaRef.current) {
+      hiddenTextareaRef.current.value = editableDivRef.current.innerHTML;
+
+      const { data, error } = await supabase
+        .from("Messages")
+        .insert({
+          // channel_id 하드코딩 할 필요 없도록 해야함
+          channel_id: "214322ba-1cbd-424c-9ef1-e4b281f71675",
+          user_id: `${userInfo?.id}`,
+          content: `${hiddenTextareaRef.current.value}`,
+        })
+        .select("*");
+
+      if (error) {
+        console.error("에러: ", error);
+        return;
+      }
+
+      editableDivRef.current.innerHTML = "";
+    } else {
+      alert("전송 오류");
       return;
     }
-
-    setInputValue("");
   };
 
   const handleDelete = async (message_id: string) => {
@@ -113,54 +129,181 @@ const Chat = () => {
   };
 
   return (
-    <div>
-      {messages.map((message) => {
-        return (
-          <div key={`${message.message_id}`}>
-            {message.user_id === userInfo?.id ? (
-              <div>
-                <p>{message?.content}</p>
-                <span>{dayjs(message.sent_at).format("YYYY-MM-DD HH:mm")}</span>
-                <button className="border-4 pointer" onClick={() => handleDelete(message.message_id)}>
-                  삭제
-                </button>
+    <>
+      <h4>실시간 채팅에 참여해보세요.</h4>
+      {isOpen ? (
+        <div id="container" className="h-[663px] w-full flex-col justify-start items-start inline-flex">
+          <div
+            id="header"
+            className="self-stretch h-16 p-5 bg-[#141415] rounded-tl-[20px] rounded-tr-[20px] flex-col justify-center items-center flex"
+          >
+            <div className="self-stretch justify-between items-center inline-flex">
+              <div className="justify-center items-center gap-1 flex">
+                <div className="w-6 h-6 p-1 justify-center items-center flex">
+                  <div className="flex-col justify-center items-center inline-flex">아이콘</div>
+                </div>
+                <div className="text-[#c4c4c4] text-sm font-normal font-['Pretendard'] leading-[21px]">오픈톡</div>
+              </div>
+              <div className="w-6 h-6 rounded-md justify-center items-center gap-1 flex">
+                <div className="justify-center items-center flex">
+                  <div className="w-5 h-5 p-1 justify-center items-center flex">
+                    <button
+                      onClick={() => setIsOpen(false)}
+                      className="flex-col justify-center items-center inline-flex"
+                    >
+                      토글 버튼
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            id="message"
+            className="self-stretch h-[454px] py-5 bg-[#141415] flex-col justify-start items-start gap-3 flex overflow-y-scroll scrollbar-hide"
+          >
+            {messages.map((message) => {
+              return (
+                <div key={`${message.message_id}`} className="w-full">
+                  {message.user_id === userInfo?.id ? (
+                    <div id="mine" className="self-stretch h-[110px] px-3 flex-col justify-center items-end flex">
+                      <div className="flex-col justify-center items-end gap-1 flex">
+                        <button className="border-4 pointer" onClick={() => handleDelete(message.message_id)}>
+                          삭제
+                        </button>
+                        <div className="w-[190px] p-3 bg-[#c3e88d] rounded-tl-[20px] rounded-tr rounded-bl-[20px] rounded-br-[20px] justify-center items-center gap-2.5 inline-flex">
+                          <div className="grow shrink basis-0 text-[#2b2b2b] text-sm font-normal font-['Pretendard'] leading-snug">
+                            <p>{message?.content}</p>
+                          </div>
+                        </div>
+                        <div className="self-stretch text-right text-[#919191] text-xs font-normal font-['Pretendard'] leading-none">
+                          <span>{dayjs(message.sent_at).format("YYYY-MM-DD HH:mm")}</span>
+                        </div>
+                      </div>
+                    </div>
+                  ) : (
+                    <div>
+                      <div id="others" className="self-stretch px-3 justify-start items-start gap-3 inline-flex">
+                        <div className="w-8 h-8 bg-[#3b3d3f] rounded-[9px] justify-center items-center flex">
+                          <div className="justify-center items-center flex">
+                            <div className="relative">
+                              <Image
+                                className="rounded-xl"
+                                src={message.Users.profile_image_url}
+                                width={32}
+                                height={32}
+                                alt="profile image"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-[190px] flex-col justify-start items-start gap-1 inline-flex">
+                          <div className="self-stretch text-[#f7f7f7] text-base font-normal font-['Pretendard'] leading-relaxed">
+                            <span>{message.Users.nickname}</span>
+                          </div>
+                          <div className="self-stretch p-3 bg-[#323334] rounded-tl rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] justify-center items-center gap-2.5 inline-flex">
+                            <div className="grow shrink basis-0 text-[#f7f7f7] text-sm font-normal font-['Pretendard'] leading-snug">
+                              <p>{message?.content}</p>
+                            </div>
+                          </div>
+                          <div className="self-stretch text-[#919191] text-xs font-normal font-['Pretendard'] leading-none">
+                            <span>{dayjs(message.sent_at).format("YYYY-MM-DD HH:mm")}</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+          </div>
+          <form action="" className="w-full" onSubmit={(evt) => handleSubmit(evt)}>
+            {user ? (
+              <div
+                id="input"
+                className="self-stretch h-[145px] w-full p-5 bg-[#141415] rounded-bl-[20px] rounded-br-[20px] flex-col justify-center items-center flex"
+              >
+                <div className="w-full h-full self-stretch justify-between items-start inline-flex">
+                  <div
+                    className="w-full h-full grow shrink basis-0 text-[#5e5e5e] text-sm font-normal mr-2 font-['Pretendard'] leading-[21px] break-all overflow-y-hidden whitespace-pre-wrap break-words"
+                    contentEditable
+                    onFocus={() => {
+                      if (editableDivRef.current) {
+                        editableDivRef.current.innerText = "";
+                      }
+                    }}
+                    onBlur={() => {
+                      if (editableDivRef.current) {
+                        // editableDivRef.current.innerText = "메시지를 입력해주세요.";
+                      }
+                    }}
+                    suppressContentEditableWarning={true}
+                    ref={editableDivRef}
+                  >
+                    메시지를 입력해주세요.
+                  </div>
+
+                  <div className="justify-center items-center flex">
+                    <div className="w-5 h-5 p-1 justify-center items-center flex">
+                      <div className="justify-center items-center flex">
+                        <textarea ref={hiddenTextareaRef} placeholder="메시지를 입력하세요" className="hidden" />
+                        <button>전송</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ) : (
-              <div>
-                <Image src={message.Users.profile_image_url} width={32} height={32} alt="profile image" />
-                <span>{message.Users.nickname}</span>
-                <p>{message?.content}</p>
-                <span>{dayjs(message.sent_at).format("YYYY-MM-DD HH:mm")}</span>
+              <div
+                id="input"
+                className="self-stretch h-[145px] w-full p-5 bg-[#141415] rounded-bl-[20px] rounded-br-[20px] flex-col justify-center items-center flex"
+              >
+                <div className="w-full h-full self-stretch justify-between items-start inline-flex">
+                  <div className="w-full h-full grow shrink basis-0 text-[#5e5e5e] text-sm font-normal mr-2 font-['Pretendard'] leading-[21px] break-all overflow-y-hidden whitespace-pre-wrap break-words">
+                    채팅을 이용하시려면 로그인을 해주세요.
+                  </div>
+                  <div className="justify-center items-center flex">
+                    <div className="w-5 h-5 p-1 justify-center items-center flex">
+                      <div className="justify-center items-center flex">
+                        <button disabled>전송</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             )}
+          </form>
+        </div>
+      ) : (
+        <div className="w-full h-16 flex-col justify-start items-start inline-flex">
+          <div className="self-stretch h-16 p-5 bg-[#141415] rounded-[20px] flex-col justify-center items-center flex">
+            <div className="self-stretch justify-between items-center inline-flex">
+              <div className="justify-center items-center gap-1 flex">
+                <div className="w-6 h-6 p-1 justify-center items-center flex">
+                  <div className="flex-col justify-center items-end inline-flex">
+                    <div className="w-[3px] h-[3px] relative bg-[#ff3e02] rounded-full">아이콘</div>
+                  </div>
+                </div>
+                <div className="text-[#c4c4c4] text-sm font-normal font-['Pretendard'] leading-[21px]">오픈톡</div>
+              </div>
+              <div className="w-6 h-6 rounded-md justify-center items-center gap-1 flex">
+                <div className="justify-center items-center flex">
+                  <div className="w-5 h-5 p-1 justify-center items-center flex">
+                    <button
+                      onClick={() => setIsOpen(true)}
+                      className="flex-col justify-center items-center inline-flex"
+                    >
+                      토글 버튼
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        );
-      })}
-      <form action="" onSubmit={(evt) => handleSubmit(evt)}>
-        {user ? (
-          <>
-            <textarea
-              placeholder="메시지를 입력하세요"
-              value={inputValue}
-              onChange={(evt) => setInputValue(evt.target.value)}
-              className="border-4"
-            />
-            <button>전송</button>
-          </>
-        ) : (
-          <>
-            <textarea
-              placeholder="채팅을 이용하려면 로그인을 해주세요."
-              value={inputValue}
-              onChange={(evt) => setInputValue(evt.target.value)}
-              className="border-4"
-              disabled
-            />
-            <button disabled>전송</button>
-          </>
-        )}
-      </form>
-    </div>
+          애니메이션 필요함
+        </div>
+      )}
+    </>
   );
 };
 
