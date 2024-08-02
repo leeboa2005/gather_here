d986eb849aa0465a1437901b9d3ecd7e4f1795c4
kybaq <flfsk12@gmail.com>
2024-08-03 01:58:07 +0900

    fix: layout.tsx 추가 #84

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import NavTabs from "@/components/MainPage/NavTab/NavTabs";
 import AllPage from "./(mainpage)/all/page";
+import NavTabs from "@/components/MainPage/NavTab/NavTabs";
 
 const MainPage = async () => {
   return (
     <div className="w-full mx-auto max-w-container-l m:max-w-container-m s:max-w-container-s">
+      <NavTabs />
       <AllPage />
     </div>
   );
