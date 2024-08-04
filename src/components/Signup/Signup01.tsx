2de02f91ca1b0f187dd58a53f12b5e0cf1bc61bf
ilovezerocokeya <147008077a@gmail.com>
2024-08-05 02:34:28 +0900

    signup폼 여백수정, 버튼색상 수정 #113

--- src/components/Signup/Signup01.tsx
+++ src/components/Signup/Signup01.tsx
@@ -28,7 +28,7 @@ const Signup01: React.FC = () => {
  return (
   <div className="s:w-[370px] s:h-[550px] w-[430px] h-[610px] relative bg-background rounded-[20px] p-4 select-none">
     <div className="absolute left-1/2 transform -translate-x-1/2 top-4 flex space-x-2">
-      <div className="w-[136px] h-10 justify-start items-center gap-2 inline-flex">
+      <div className="w-[136px] s:h-18 h-20 justify-start items-center gap-2 inline-flex">
         <div className="w-10 h-10 p-2.5 rounded-[11px] border border-[#c3e88d] flex-col justify-center items-center gap-2.5 inline-flex">
           <div className="self-stretch text-center text-[#c3e88d] text-sm font-medium font-['Pretendard'] leading-[21px]">1</div>
         </div>
@@ -40,13 +40,13 @@ const Signup01: React.FC = () => {
         </div>
       </div>
     </div>
-    <div className="text-center text-2xl font-medium text-[#ffffff] leading-9 mt-16">
+    <div className="text-center text-2xl font-medium text-[#ffffff] leading-9 s:mt-16 mt-20">
       어떤 일을 하고 계신가요?
     </div>
     <div className="text-center text-[#9a9a9a] mt-2">
       직무와 관련된 스터디 및 프로젝트, <br /> 다양한 IT행사를 추천해 드려요.
     </div>
-    <div className="grid grid-cols-3 gap-1 s:mt-4 mt-8 s:w-[335px] w-[370px] mx-auto">
+    <div className="grid grid-cols-3 gap-1 s:mt-4 mt-6 s:w-[335px] w-[370px] mx-auto">
       {['프론트엔드', '백엔드', 'IOS', '안드로이드', '데브옵스', '기획자', '디자인', '마케팅', 'PM'].map((job) => (
         <button
           key={job}
