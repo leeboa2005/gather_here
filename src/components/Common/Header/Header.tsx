5a1116f68234601a052daab8217741ad4f2ebab4
ilovezerocokeya <147008077a@gmail.com>
2024-08-04 15:38:33 +0900

    update: 로그인폼,회원가입폼 수정 #105

--- src/components/Common/Header/Header.tsx
+++ src/components/Common/Header/Header.tsx
@@ -145,7 +145,7 @@ const Header: React.FC = () => {
           {/* 백드롭 추가 */}
           <div className="fixed inset-0 bg-black opacity-80 z-40" onClick={handleCloseLoginModal}></div>
           {/* 모달 추가 */}
-          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-background rounded-lg p-4 z-50">
+          <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-background rounded-[20px] p-4 z-50">
           <button onClick={handleCloseLoginModal} className="ml-auto mt-1 mr-1 block text-right p-1 text-3xl text-[fontWhite] hover:text-[#777]">
               &times;
           </button>
