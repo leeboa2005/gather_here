a8590f404d2eaf48f65ec34c0d03008037368044
ilovezerocokeya <147008077a@gmail.com>
2024-08-06 15:40:41 +0900

    del: 페이지전환 로딩애니메이션 삭제 #143

---
+++ src/components/MainPage/InfiniteScroll/EventsInfiniteScroll.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+
+import AdCard from "@/components/MainPage/AdCard/AdCard";
+import loadingSpinner from "../../../assets/loadingSpinner.json";
+import LottiAnimation from "@/components/Common/Loading/LottiAnimation";
+import ItEventCardLong from "../PageContent/ItEvent/Card/ItEventCardLong";
+import { Tables } from "@/types/supabase";
+
+interface InfiniteScrollComponentProps {
+  posts: Tables<"IT_Events">[];
+  hasMore: boolean;
+  loadMorePosts: () => Promise<void>;
+}
+
+const EventsInfiniteScrollComponent: React.FC<InfiniteScrollComponentProps> = ({
+  posts = [],
+  hasMore,
+  loadMorePosts,
+}) => {
+  return (
+    <InfiniteScroll
+      dataLength={posts.length}
+      next={loadMorePosts}
+      hasMore={hasMore}
+      loader={
+        <div className="flex justify-center items-center w-full" style={{ marginTop: "20px" }}>
+          <LottiAnimation animationData={loadingSpinner} size="50px" />
+        </div>
+      }
+      endMessage={<p style={{ textAlign: "center", color: "white" }}>모든 포스트를 불러왔습니다.</p>}
+    >
+      {posts.map((post, index) => (
+        <React.Fragment key={`${post.event_id}_${index}`}>
+          <ItEventCardLong post={post} />
+          {(index + 1) % 5 === 0 && <AdCard key={`ad_${index}`} />}
+        </React.Fragment>
+      ))}
+    </InfiniteScroll>
+  );
+};
+
+export default EventsInfiniteScrollComponent;
