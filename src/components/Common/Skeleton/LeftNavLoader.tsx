27951c5bf80d73b863540782b54265f9d4b28685
leeboa2005 <leeboa2005@naver.com>
2024-08-10 21:56:16 +0900

    update : css 코드 수정

--- src/components/Common/Skeleton/LeftNavLoader.tsx
+++ src/components/Common/Skeleton/LeftNavLoader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LeftNavLoader: React.FC = () => {
   return (
     <div className="animate-pulse flex items-center gap-3 mb-5">
-      <div className="w-12 h-12 before:rounded-xl bg-fillLight"></div>
+      <div className="w-12 h-12 rounded-xl bg-fillLight"></div>
       <div>
         <div className="h-5 bg-fillLight rounded w-12 mb-2"></div>
         <div className="h-4 bg-fillLight rounded w-20"></div>
