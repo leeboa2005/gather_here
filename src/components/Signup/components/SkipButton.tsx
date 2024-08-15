62d1e7c5b1ed92cbf2ce110e449a11953bab54a4
ilovezerocokeya <147008077a@gmail.com>
2024-08-15 19:43:47 +0900

    update: 브라우저알림창->sweetAlert2교체 #210

--- src/components/Signup/components/SkipButton.tsx
+++ src/components/Signup/components/SkipButton.tsx
@@ -8,7 +8,7 @@ const SkipButton: React.FC<SkipButtonProps> = ({ onSkip }) => {
   return (
     <button 
       onClick={onSkip} 
-      className="absolute top-10 right-9 text-[#c4c4c4] text-sm font-medium hover:text-[#f0f0f0] hover:bg-[#5e5e5e]"
+      className="absolute top-10 right-9 text-[#ffffff] text-sm font-medium hover:text-[#777]"
     >
       건너뛰기
     </button>
