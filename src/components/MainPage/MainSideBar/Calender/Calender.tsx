8d969e90f9157faa2baa3792eed941c15e0b346a
kybaq <flfsk12@gmail.com>
2024-08-17 19:52:17 +0900

    update: PrCard 이미지 슬라이더 추가 #223

--- src/components/MainPage/MainSideBar/Calender/Calender.tsx
+++ src/components/MainPage/MainSideBar/Calender/Calender.tsx
@@ -83,7 +83,7 @@ const Calender: NextPage = () => {
 
   return (
     <div className="custom_calender mb-6">
-      <h4 className="flex  text-fontWhite">
+      <h4 className="flex text-labelNormal mb-3">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-1">
           <path
             fillRule="evenodd"
@@ -92,7 +92,7 @@ const Calender: NextPage = () => {
             fill="#C3E88D"
           />
         </svg>
-        <p className="mb-4">HOT한 IT 행사 놓치지 마세요</p>
+        HOT한 IT 행사 놓치지 마세요
       </h4>
       <FullCalendar
         // 공통 옵션
