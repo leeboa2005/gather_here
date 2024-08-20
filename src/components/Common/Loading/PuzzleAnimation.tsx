8b068258ab8de5086ae4548ece417f2b2d576823
leeboa2005 <leeboa2005@naver.com>
2024-08-20 13:40:57 +0900

    update: 불필요한 이미지 및 코드 애니메이션 코드 삭제 #241

--- src/components/Common/Loading/PuzzleAnimation.tsx
+++ src/components/Common/Loading/PuzzleAnimation.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const PuzzleAnimation = () => {
   return (
-    <div className="relative w-[324px] h-[72px]">
+    <div className="relative w-[324px] h-[72px] s:w-[300px] s:h-[67px]">
       <Image src="/assets/gif/Loading-Large.gif" alt="Puzzle Icon" layout="fill" objectFit="contain" />
     </div>
   );
