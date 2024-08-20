d303795cd2680dd2f8aa2334c3fa46f6141c4f41
Eunyoung-Jo <23ailce@naver.com>
2024-08-20 15:54:13 +0900

    style: 텍스트에디터 #245

--- src/components/MainDetail/ReactQuillEditor.tsx
+++ src/components/MainDetail/ReactQuillEditor.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
-// import "react-quill/dist/quill.bubble.css";
-// import "react-quill/dist/quill.core.css";
+import "react-quill/dist/quill.bubble.css";
+import "react-quill/dist/quill.core.css";
 import "./custom-quill.css";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
